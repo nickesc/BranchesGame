b094162f46ac2176d5da730ee26134187a484d00
Nick <nescobar@oxy.edu>
2021-12-06 14:33:29 -0800

    adding fresnel

--- Assets/PassingTownTrigger.cs
+++ Assets/PassingTownTrigger.cs
@@ -29,11 +29,11 @@ public class PassingTownTrigger : MonoBehaviour
             {
                 if (disembarkBoatPass == 0)
                 {
-                    MngrScript.Instance.PushSubtitle("Wonder what that is...", "silenceFive", false);
+                    MngrScript.Instance.PushSubtitle("Wonder what came in this time...", "silenceFive", false);
                 }
                 if (disembarkBoatPass == 1)
                 {
-                    MngrScript.Instance.PushSubtitle("Oh well, better get up.");
+                    MngrScript.Instance.PushSubtitle("Something to worry about tomorrow", "silenceFive", false);
                 }
 
                 disembarkBoatPass++;
